987ed089a1757b7c7fc38a87214d97cc7348d0a0
ToanTrinh2004 <2251120326@ut.edu.vn>
2026-08-20 19:56:58 +0700

    fix import redis in dependency

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -2,28 +2,29 @@ from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import uuid
 
-
 from app.core.sercurity import decode_token
-from app.db import redis_client
+from app.db.redis_client import get_redis
 from app.db.session import get_db
 from app.modules.users.repository import UserRepository
 
 
 bearer_scheme = HTTPBearer()
 
-async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme), db=Depends(get_db)):
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+    db=Depends(get_db),
+    redis_client=Depends(get_redis),
+):
     token = credentials.credentials
     try:
         payload = decode_token(token)
     except:
         raise HTTPException(status_code=401, detail="Invalid Token")
 
-    ## check is token in blacklist
     is_blacklist = await redis_client.get(f"blacklist:{token}")
     if is_blacklist:
         raise HTTPException(status_code=401, detail="Token was revoke")
-    
-    
+
     user_id = payload.get("sub")
     repo = UserRepository(db)
     user = await repo.get_by_id(uuid.UUID(user_id))

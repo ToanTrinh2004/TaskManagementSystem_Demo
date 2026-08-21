1cac32fd9b7622ca91212eff139ae5e535572463
ToanTrinh2004 <2251120326@ut.edu.vn>
2026-08-21 21:54:21 +0700

    add unique constrain for ProjectMember

--- app/modules/project_members/model.py
+++ app/modules/project_members/model.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 from enum import Enum as PyEnum
-from sqlalchemy import DateTime, Enum, ForeignKey, func
+from sqlalchemy import DateTime, Enum, ForeignKey, UniqueConstraint, func
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 from app.db.session import Base
@@ -14,6 +14,10 @@ class ProjectRole(str, PyEnum):
 
 class ProjectMember(Base):
     __tablename__ = "project_members"
+
+    __table_args__ = (UniqueConstraint("project_id","user_id",name="uq_project_member_project_user",),)
+
+    
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),primary_key=True, default=uuid.uuid4)
     project_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),ForeignKey("projects.id"), nullable=False)
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"),nullable=False)
